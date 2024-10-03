1d898e1ad799d3fb681e8549bba0b911e95197e3
Abdelrahman Saber <109411513+AbdelrahmanSaber74@users.noreply.github.com>
2024-10-03 15:37:27 +0300

    feat: Implement Add, Update, and Delete functionalities for categories - Added the ability to add new categories with validation. - Implemented the update functionality to modify existing categories. - Added delete functionality to remove categories. - Updated the corresponding views and controller methods. - Ensured proper error handling and validation."

--- Bookify.Web/Controllers/CategoriesController.cs
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -1,23 +1,103 @@
-﻿using Bookify.Web.Repositories.Categories;
+﻿using Bookify.Web.Core.Models;
+using Bookify.Web.DTO;
+using Bookify.Web.Repositories.Categories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Web.Controllers
 {
     public class CategoriesController : Controller
     {
-
         private readonly ICategoriesRepo _categoriesRepo;
 
         public CategoriesController(ICategoriesRepo categoriesRepo)
         {
             _categoriesRepo = categoriesRepo;
         }
-       //Todo : use view model
+
         public async Task<IActionResult> Index()
         {
-            var categories =  await _categoriesRepo.GetAllCategoriesAsync();
+            var categories = await _categoriesRepo.GetAllCategoriesAsync();
             return View(categories);
         }
 
+        public IActionResult Create()
+        {
+            return View("AddCategory");
+        }
 
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category = await _categoriesRepo.GetCategoryByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var categoryDto = MapToDto(category);
+            return View("EditCategory", categoryDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(CategoryDTO categoryDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoryDto);
+            }
+
+            var newCategory = new Category
+            {
+                Name = categoryDto.Name,
+                CreatedOn = DateTime.Now,
+                IsDeleted = categoryDto.IsDeleted,
+            };
+
+            await _categoriesRepo.AddCategoryAsync(newCategory);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(CategoryDTO categoryDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoryDto);
+            }
+
+            var existingCategory = await _categoriesRepo.GetCategoryByIdAsync(categoryDto.Id);
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            existingCategory.Name = categoryDto.Name;
+            existingCategory.IsDeleted = categoryDto.IsDeleted;
+            existingCategory.LastUpdatedOn = DateTime.Now;
+
+            await _categoriesRepo.UpdateCategoryAsync(existingCategory);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var existingCategory = await _categoriesRepo.GetCategoryByIdAsync(id);
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            await _categoriesRepo.DeleteCategoryAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private CategoryDTO MapToDto(Category category)
+        {
+            return new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name,
+                IsDeleted = category.IsDeleted,
+            };
+        }
     }
 }
--- Bookify.Web/DTO/CategoryDTO.cs
+++ Bookify.Web/DTO/CategoryDTO.cs
@@ -1,11 +1,21 @@
-﻿namespace Bookify.Web.Core.DTOs
+﻿namespace Bookify.Web.DTO
 {
     public class CategoryDTO
     {
+
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category Name is required")]
+        [StringLength(100, ErrorMessage = "Category Name cannot be longer than 100 characters")]
         public string Name { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public DateTime? LastUpdatedOn { get; set; }
+
+        public bool IsDeleted { get; set; } // Soft delete indicator
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedOn { get; set; } = DateTime.Now; // Tracks creation time
+
+        [DataType(DataType.DateTime)]
+        public DateTime? LastUpdatedOn { get; set; }  // Tracks last update time
     }
+
 }
