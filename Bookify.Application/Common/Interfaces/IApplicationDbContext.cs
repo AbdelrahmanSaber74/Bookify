f0b6383c97e99ed48196eff5c6bbe7600eb9f3f8
Abdelrahman Saber <109411513+AbdelrahmanSaber74@users.noreply.github.com>
2024-12-01 10:02:43 +0200

    Add files via upload

--- Bookify.Application/Common/Interfaces/IApplicationDbContext.cs
+++ Bookify.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,22 +1,17 @@
-﻿using Bookify.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-
-namespace Bookify.Application.Common.Interfaces
+﻿namespace Bookify.Application.Common.Interfaces;
+public interface IApplicationDbContext
 {
-	public interface IApplicationDbContext
-	{
-		DbSet<Category> Categories { get; set; }
-		DbSet<Author> Authors { get; set; }
-		DbSet<Book> Books { get; set; }
-		DbSet<BookCategory> BookCategories { get; set; }
-		DbSet<BookCopy> BookCopies { get; set; }
-		DbSet<Area> Areas { get; set; }
-		DbSet<Governorate> Governorates { get; set; }
-		DbSet<Rental> Rentals { get; set; }
-		DbSet<RentalCopy> RentalCopies { get; set; }
-		DbSet<Subscriber> Subscribers { get; set; }
-		DbSet<Subscription> Subscriptions { get; set; }
+    public DbSet<Area> Areas { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<BookCategory> BookCategories { get; set; }
+    public DbSet<BookCopy> BookCopies { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Governorate> Governorates { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
+    public DbSet<RentalCopy> RentalCopies { get; set; }
+    public DbSet<Subscriber> Subscribers { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
 
-		Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-	}
+    int SaveChanges();
 }
--- Bookify.Application/Common/Models/PaginatedList.cs
+++ Bookify.Application/Common/Models/PaginatedList.cs
@@ -1,27 +1,27 @@
 ﻿namespace Bookify.Application.Common.Models
 {
-	public class PaginatedList<T> : List<T>
-	{
-		public int PageIndex { get; private set; }
-		public int TotalPages { get; private set; }
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageNumber { get; private set; }
+        public int TotalPages { get; private set; }
 
-		public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
-		{
-			PageIndex = pageIndex;
-			TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
-			AddRange(items);
-		}
+            AddRange(items);
+        }
 
-		public bool HasPreviousPage => PageIndex > 1;
+        public bool HasPreviousPage => PageNumber > 1;
 
-		public bool HasNextPage => PageIndex < TotalPages;
+        public bool HasNextPage => PageNumber < TotalPages;
 
-		public static  PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
-		{
-			var count =  source.Count();
-			var items =  source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
-			return new PaginatedList<T>(items, count, pageIndex, pageSize);
-		}
-	}
+        public static PaginatedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
 }
